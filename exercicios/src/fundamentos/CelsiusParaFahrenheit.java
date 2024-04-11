f1cd48138f4363ced52006eb44bb2690e64531ac
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-10 21:12:12 -0400

    Exercício Celsius para Fahrenheit

---
+++ exercicios/src/fundamentos/CelsiusParaFahrenheit.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class CelsiusParaFahrenheit {
+	public static void main(String[] args) {
+		double F;
+		double C;
+		
+		C = 7;
+			
+			F = (9 * C / 5) + 32;
+			
+			System.out.println("A temperatura de " + C +"º Celsius é igual a " + F + "º Fahrenheit.");
+			
+	}
+
+}
