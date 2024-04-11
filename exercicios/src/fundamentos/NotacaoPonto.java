c8873d1c145f25c18c3e9baebe18d451a6662505
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-11 12:17:49 -0400

    Notacao Ponto

---
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class NotacaoPonto {
+
+	public static void main(String[] args) {
+		
+		double a = 2.3;
+		String s = "Bom dia X"; //S maiúsculo
+		// String nao é um tipo primitivo e não é uma palavra reservada
+		
+		System.out.println(s);
+		
+		s = s.replace("X", "Senhor");
+		s = s.toUpperCase();
+		s = s.concat(" !!!");
+		System.out.println(s);
+		
+		System.out.println("Leo".toUpperCase());
+		
+	}
+}
