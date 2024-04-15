66599f023139543e9df1c7ceb46e45da44583948
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-15 12:50:32 -0400

    Conversao de numero para string

---
+++ exercicios/src/fundamentos/ConversaoNumeroString.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class ConversaoNumeroString {
+	
+	public static void main(String[] args) {
+		 //usando wrapper
+		Integer num1 = 10000;
+		System.out.println(num1.toString());
+		System.out.println(num1.toString().length()); //apos converter para string, é possível acessar todos os métodos da string
+		
+		//diretamente do tipo integer
+		int num2 = 10000; 
+		System.out.println(Integer.toString(num2));
+		System.out.println(Integer.toString(num2).length());
+		
+		//concatenando com string vazia
+		System.out.println("" + num2);
+		System.out.println(("" + num2).length());
+		
+	}
+
+}
