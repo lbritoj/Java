bb46d7f799ec1ea8787fee84559915dced02b96c
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-21 11:05:36 -0400

    desafio aritmeticos

---
+++ exercicios/src/fundamentos/DesafioAritmeticos.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class DesafioAritmeticos {
+	
+	public static void main(String[] args) {
+		
+		double a1 = (6 * (3+2));
+		double a2 = Math.pow(a1, 2);
+		double a3 = (a2 / (3*2));
+		
+		double b1 = (((1-5)*(2-7)) / 2);
+		double b2 = Math.pow(b1, 2);
+		
+		double p1 = (a3 - b2);
+		double p2 = Math.pow(p1, 3);
+		
+		double q = Math.pow(10, 3);
+		
+		double resposta = p2 /q ;
+		
+		System.out.println(resposta);
+		
+		
+		
+	}
+
+}
