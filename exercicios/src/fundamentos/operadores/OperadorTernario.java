53803fbe4bd4a795a88f1294efe2c23c4e67bfc7
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-22 17:00:34 -0400

    Equals para comparar Strings

--- exercicios/src/fundamentos/operadores/OperadorTernario.java
+++ exercicios/src/fundamentos/operadores/OperadorTernario.java
@@ -5,8 +5,8 @@ public class OperadorTernario {
 	public static void main(String[] args) {
 		
 		double media = 5.6;
-		String resultadoParcial = media >= 5.0 ? "em reculperação" : "reprovado";
-		String resultado = media >= 7.0? "aprovado" : resultadoParcial;
+		String resultadoParcial = media >= 5.0 ? "em recuperação." : "reprovado.";
+		String resultado = media >= 7.0? "aprovado." : resultadoParcial;
 		
 		System.out.println("O aluno está: " + resultado);
 	}
