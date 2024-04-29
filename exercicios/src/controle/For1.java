b4cd146d4badf71e074495814ecd3b9357adebfd
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-29 19:37:39 -0400

    While

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,20 @@
+package controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int contador = 1; contador <= 10; contador +=1) {
+			System.out.printf("i = %d\n", contador);
+		}
+		
+		int x = 2;
+		for (; x < 10; ) {
+			System.out.println("x = " + x);
+			x++;
+		}
+
+	}
+
+}
--- exercicios/src/controle/IfElse.java
+++ exercicios/src/controle/IfElse.java
@@ -6,17 +6,15 @@ public class IfElse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	String valor = JOptionPane.showInputDialog("Informe o número:");
-	int numero = Integer.parseInt(valor);
-	
-	if(numero % 2 == 0) {
-		System.out.println("Número par");
-	}
-	
-	else {
-		System.out.println("Número impar");
-	}
+
+		String valor = JOptionPane.showInputDialog("Informe o número:");
+		int numero = Integer.parseInt(valor);
+
+		if (numero % 2 == 0) {
+			System.out.println("Número par");
+		} else {
+			System.out.println("Número impar");
+		}
 
 	}
 
